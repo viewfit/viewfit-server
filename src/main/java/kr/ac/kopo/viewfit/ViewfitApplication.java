bcd6bb01fa53a35f7d9138474ef97f1a7a02e36a
SSY <993721@naver.com>
2025-09-08 14:27:30 +0900

    first commit

---
+++ src/main/java/kr/ac/kopo/viewfit/ViewfitApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.viewfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViewfitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ViewfitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/kopo/viewfit/ViewfitApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.viewfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ViewfitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
