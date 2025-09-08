17fd97ae5908744bb9c4b3ced370200c4e276c28
SSY <993721@naver.com>
2025-09-08 16:49:07 +0900

    DB 구축 및 env 설정 완료

---
+++ src/main/java/kr/ac/kopo/viewfit/controller/RootController.java
@@ -0,0 +1,15 @@
+package kr.ac.kopo.viewfit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("")
+public class RootController {
+
+    @GetMapping("")
+    public String index() {
+        return "";
+    }
+}
