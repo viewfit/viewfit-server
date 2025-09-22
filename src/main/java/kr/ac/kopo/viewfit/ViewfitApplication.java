adc915d5db89b7d23d871a0834a0fcf8662ef464
SSY <993721@naver.com>
2025-09-23 00:52:41 +0900

    CORS 세팅 완료, Client 통신 확인

--- src/main/java/kr/ac/kopo/viewfit/ViewfitApplication.java
+++ src/main/java/kr/ac/kopo/viewfit/ViewfitApplication.java
@@ -2,6 +2,8 @@ package kr.ac.kopo.viewfit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 @SpringBootApplication
 public class ViewfitApplication {
